f2737c844c18930af9aa754588e1fee003a17b03
Jihun Song <songjihun940409@gmail.com>
2022-11-13 20:52:45 +0900

    2.1 TAILWIND - Test Drive Part Two

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex flex-col space-y-5 bg-slate-400 py-10 px-5">
-      <div className="rounded-2xl bg-white p-6 shadow-xl">
+    <div className="flex flex-col space-y-5 bg-slate-400 py-20 px-20">
+      <div className="rounded-3xl bg-white p-6 shadow-xl">
         <span className="text-3xl font-semibold">Select Item</span>
         <div className="my-2 flex justify-between">
           <span className="text-gray-500">Grey Chair</span>
@@ -19,9 +19,30 @@ export default function Home() {
           Check Out
         </div>
       </div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
+      <div className="overflow-hidden rounded-3xl bg-white shadow-xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-2xl text-white">Profile</span>
+        </div>
+        <div className="relative -top-5 rounded-3xl bg-white p-6">
+          <div className="relative -top-16 flex items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 rounded-full bg-red-400" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+          </div>
+          <div className=" relative  -mt-10 -mb-5 flex flex-col items-center">
+            <span className="text-lg font-medium">Tony Molloy</span>
+            <span className="text-sm text-gray-500">미국</span>
+          </div>
+        </div>
+      </div>
+      <div className="rounded-3xl bg-white p-6 shadow-xl"></div>
+      <div className="rounded-3xl bg-white p-6 shadow-xl"></div>
     </div>
   );
 }
