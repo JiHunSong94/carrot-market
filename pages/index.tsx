4b824da6ed337951de72f32f5818c3b321d0f06a
Jihun Song <songjihun940409@gmail.com>
2022-11-13 14:17:46 +0900

    1.1 TailwindCss Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
 export default function Home() {
-  return null;
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-black">it works</h1>
+    </div>
+  );
 }
