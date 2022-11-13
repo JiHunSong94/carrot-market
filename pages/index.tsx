cbaa801adda7b4b82b12ab7a39c74e651b6b1a21
Jihun Song <songjihun940409@gmail.com>
2022-11-13 22:27:47 +0900

    2.3 TAILWIND - Modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,9 +15,9 @@ export default function Home() {
           <span>Total</span>
           <span className="font-semibold">$9</span>
         </div>
-        <div className="mx-auto mt-5 w-1/2 rounded-xl bg-blue-500 p-2 text-center text-white">
+        <button className="mx-auto mt-5 w-3/4 rounded-xl bg-blue-500 p-2 text-center text-white hover:bg-teal-500 hover:text-black focus:bg-red-500 active:bg-yellow-500">
           Check Out
-        </div>
+        </button>
       </div>
       <div className="overflow-hidden rounded-3xl bg-white shadow-xl">
         <div className="bg-blue-500 p-6 pb-14">
@@ -54,10 +54,10 @@ export default function Home() {
           <span className="font-demium mb-1.5 text-xl">Swoon Lounge</span>
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex items-center justify-between">
-            <div>
-              <input type="radio" />
-              <input type="radio" />
-              <input type="radio" />
+            <div className="space-x-2">
+              <button className="h-5 w-5 rounded-full bg-yellow-500"></button>
+              <button className="h-5 w-5 rounded-full bg-indigo-500"></button>
+              <button className="h-5 w-5 rounded-full bg-teal-500"></button>
             </div>
             <div className="flex items-center space-x-5">
               <button className="flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200 p-2.5 text-xl text-gray-500">
