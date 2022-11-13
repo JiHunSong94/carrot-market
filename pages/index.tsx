c8f0f6adb6691141acf83b18680f5ab3e6838a36
Jihun Song <songjihun940409@gmail.com>
2022-11-13 18:11:04 +0900

    2.0 TAILWIND - Test Drive Part One

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,27 @@
 export default function Home() {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-black">it works</h1>
+    <div className="flex flex-col space-y-5 bg-slate-400 py-10 px-5">
+      <div className="rounded-2xl bg-white p-6 shadow-xl">
+        <span className="text-3xl font-semibold">Select Item</span>
+        <div className="my-2 flex justify-between">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Tooly Table</span>
+          <span className="font-semibold">$89</span>
+        </div>
+        <div className=" mt-2 flex justify-between border-t-2 border-dashed pt-2">
+          <span>Total</span>
+          <span className="font-semibold">$9</span>
+        </div>
+        <div className="mx-auto mt-5 w-1/2 rounded-xl bg-blue-500 p-2 text-center text-white">
+          Check Out
+        </div>
+      </div>
+      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
+      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
+      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
     </div>
   );
 }
