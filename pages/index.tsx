9e5ca40dc0b8ac4c18f41aa5dc62ced36820dd4f
Jihun Song <songjihun940409@gmail.com>
2022-11-14 23:11:30 +0900

    3.0 TW PRACTICE - Auth part One

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="bg-[url('/vercel.svg')] dark:md:hover:bg-teal-400">
-      <h2 className="text-[91px] text-[#000]">Hello</h2>
-    </div>
-  );
+  return null;
 }
