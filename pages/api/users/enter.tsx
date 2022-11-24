c0f9acf17a21d01fc4a656beb0e4f55808034705
Jihun Song <songjihun940409@gmail.com>
2022-11-24 20:21:44 +0900

    7.4 AUTHENTICATION - Sending Email

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,7 +2,9 @@ import client from "@libs/server/client";
 import withhHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import twilio from "twilio";
+import mail from "@sendgrid/mail";
 
+mail.setApiKey(process.env.SENDGRID_API_KEY!);
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
@@ -36,6 +38,15 @@ async function handler(
       body: `Your login token is ${payload}`,
     });
     console.log(message);
+  } else if (email) {
+    const email = await mail.send({
+      from: "songjihun940409@gmail.com",
+      to: "songjihun940409@gmail.com",
+      subject: "Your Carrot Market Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email);
   }
   return res.json({ ok: true });
 }
