f2bf4a4827dbafd286ba9333e87293be506a05e9
Jihun Song <songjihun940409@gmail.com>
2022-11-26 20:12:08 +0900

    7.8 AUTHENTICATION - Cookie & Token Logic Edit

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -2,20 +2,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 import withhHandler, { ResponseType } from "@libs/server/withHandler";
 import { withIronSessionApiRoute } from "iron-session/next";
 import client from "@libs/server/client";
-
-declare module "iron-session" {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    };
-  }
-}
+import { withApiSession } from "@libs/server/withSession";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  console.log(req.session.user);
   const profile = await client.user.findUnique({
     where: { id: req.session.user?.id },
   });
@@ -23,8 +15,4 @@ async function handler(
   res.json({ ok: true, profile });
 }
 
-export default withIronSessionApiRoute(withhHandler("GET", handler), {
-  cookieName: "carrotsession",
-  password:
-    "2413544357648712364871632131asdfasdgarqweqew13rw53qhw45532rwtrhq45q3rfqr",
-});
+export default withApiSession(withhHandler("GET", handler));
