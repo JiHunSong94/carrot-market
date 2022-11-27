6c705d9f17ac017082029f9ac61af1953e629032
Jihun Song <songjihun940409@gmail.com>
2022-11-28 01:36:49 +0900

    8.1 AUTHORIZATION - useUser Hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,17 @@ import Link from "next/link";
 import FloatingButton from "@components/floating-button";
 import Item from "@components/item";
 import Layout from "@components/layout";
+import useUser from "@libs/client/useUser";
+import Head from "next/head";
 
 export default function Home() {
+  const user = useUser();
+  console.log(user);
   return (
     <Layout title="홈" hasTabBar>
+      <Head>
+        <title>Home</title>
+      </Head>
       <div className="flex flex-col space-y-5 py-4">
         {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
           <Item
