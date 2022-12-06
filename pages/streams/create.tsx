088983f1da9d6834dcc3c69536a17a1ed8224978
Jihun Song <songjihun940409@gmail.com>
2022-12-06 16:15:51 +0900

    12.0 STREAMS - Upload Form

--- pages/streams/create.tsx
+++ pages/streams/create.tsx
@@ -2,22 +2,55 @@ import Button from "@components/button";
 import Input from "@components/input";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import useMutation from "@libs/client/useMutation";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+interface CreateForm {
+  name: string;
+  price: string;
+  description: string;
+}
 
 export default function Create() {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<CreateForm>();
+  const onValid = (form: CreateForm) => {
+    if (loading) return;
+    createStream(form);
+  };
+  const [createStream, { data, loading }] = useMutation(`/api/streams`);
+  console.log(data);
+  /* useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/streams/${id})
+    }
+  }, [data]); */
   return (
     <Layout canGoBack title="Go Live">
-      <form className="space-y-4 py-10 px-4">
-        <Input label="Name" name="name" type="text" required />
+      <form onSubmit={handleSubmit(onValid)} className="space-y-4 py-10 px-4">
         <Input
+          register={register("name", { required: true })}
+          label="Name"
+          name="name"
+          type="text"
+          required
+        />
+        <Input
+          register={register("price", { required: true })}
           label="Price"
           name="price"
           kind="price"
           type="text"
-          placeholder="0.00"
           required
         />
-        <TextArea label="Description" name="description" />
-        <Button text="Go live" />
+        <TextArea
+          register={register("description", { required: true })}
+          label="Description"
+          name="description"
+        />
+        <Button text={loading ? "Loading..." : "Go live"} />
       </form>
     </Layout>
   );
