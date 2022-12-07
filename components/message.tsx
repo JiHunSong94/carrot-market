1f89508624a289650453c7380694dda74163f2b4
Jihun Song <songjihun940409@gmail.com>
2022-12-07 15:13:58 +0900

    12.4 STREAMS - Mutations & Refresh

--- components/message.tsx
+++ components/message.tsx
@@ -3,15 +3,20 @@ import { cls } from "@libs/client/utils";
 interface MessageProps {
   message: string;
   reversed?: boolean;
+  avatarUrl?: string;
 }
 
-export default function Message({ message, reversed }: MessageProps) {
+export default function Message({
+  message,
+  reversed,
+  avatarUrl,
+}: MessageProps) {
   return (
     <div>
       <div
         className={cls(
-          "flex items-start",
-          reversed ? "flex-row-reverse space-x-reverse" : "space-x-2"
+          "flex items-start space-x-2",
+          reversed ? "flex-row-reverse space-x-reverse" : ""
         )}
       >
         <div className="h-8 w-8 rounded-full bg-slate-400" />
--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -35,7 +35,10 @@ export default function StreamDetail() {
   const { user } = useUser();
   const { register, handleSubmit, reset } = useForm<MessageForm>();
   const { data, mutate } = useSWR<StreamResponse>(
-    router.query.id ? `/api/streams/${router.query.id}` : null
+    router.query.id ? `/api/streams/${router.query.id}` : null,
+    {
+      refreshInterval: 1000,
+    }
   );
   const [sendMessage, { loading, data: sendMessageData }] = useMutation(
     `/api/streams/${router.query.id}/messages`
@@ -43,13 +46,23 @@ export default function StreamDetail() {
   const onValid = (form: MessageForm) => {
     if (loading) return;
     reset();
-    sendMessage(form);
+    mutate(
+      (prev) =>
+        prev &&
+        ({
+          ...prev,
+          stream: {
+            ...prev.stream,
+            messages: [
+              ...prev.stream.messages,
+              { id: Date.now(), message: form.message, user: { ...user } },
+            ],
+          },
+        } as any),
+      false
+    );
+    //sendMessage(form);
   };
-  useEffect(() => {
-    if (sendMessageData && sendMessageData.ok) {
-      mutate();
-    }
-  }, [sendMessageData, mutate]);
 
   return (
     <Layout canGoBack>
@@ -70,7 +83,7 @@ export default function StreamDetail() {
             <Message
               key={message.id}
               message={message.message}
-              reversed={message.user.id === user.id}
+              reversed={message.user.id === user?.id}
             />
           ))}
         </div>
