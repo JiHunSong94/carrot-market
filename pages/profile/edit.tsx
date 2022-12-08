72a01ee9c11f1b82189c147e9e9ddeffd352849b
Jihun Song <songjihun940409@gmail.com>
2022-12-08 21:56:17 +0900

    13.3 CLOUDFLARE IMAGES - Cloudflare Upload

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -38,11 +38,14 @@ export default function EditProfile() {
         message: "Email OR Phone number are required. You need to choose one.",
       });
     }
-    if (avatar && avatar.length > 0) {
-      // ask for CF URL
-      const cloudflareRequest = await (await fetch("/api/files")).json();
-      console.log(cloudflareRequest);
-      //uploadfile to CF URL
+    if (avatar && avatar.length > 0 && user) {
+      const { id, uploadURL } = await (await fetch("/api/files")).json();
+      const form = new FormData();
+      form.append("file", avatar[0], user?.id + "");
+      await fetch(uploadURL, {
+        method: "POST",
+        body: form,
+      });
       return;
       editProfile({
         name,
