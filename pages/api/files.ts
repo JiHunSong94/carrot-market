d98a51d305b78c92182f577da5fa6e2d3e5afe7a
Jihun Song <songjihun940409@gmail.com>
2022-12-08 15:49:03 +0900

    13.1 CLOUDFLARE IMAGES - Cloudflare Setup

---
+++ pages/api/files.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withIronSessionApiRoute } from "iron-session/next";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  res.json({ ok: true, url: "" });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    handler,
+  })
+);
