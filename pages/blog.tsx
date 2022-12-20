fe006ca3cc0ccfbdfb82cb4dacfe0635e0514794
Jihun Song <songjihun940409@gmail.com>
2022-12-20 17:40:23 +0900

    16.9 NEXTJS DEEP DIVE - Blog Section

---
+++ pages/blog.tsx
@@ -0,0 +1,12 @@
+import Layout from "@components/layout";
+
+export default function Blog() {
+  return (
+    <Layout title="Blog" seoTitle="Blog">
+      <h1 className="text-lg font-semibold">Latest Posts</h1>
+      <ul>
+        <li>Welcome everyone!</li>
+      </ul>
+    </Layout>
+  );
+}
