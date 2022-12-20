ff8cdf84dfeb1598abf868bd7e7cf2429d16772f
Jihun Song <songjihun940409@gmail.com>
2022-12-20 21:56:37 +0900

    16.11 NEXTJS DEEP DIVE - getStaticPaths part One

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const Post: NextPage = () => {
+  return <h1>hi</h1>;
+};
+
+export function getStaticProps() {
+  return {
+    props: {},
+  };
+}
+export default Post;
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,27 +2,32 @@ import Layout from "@components/layout";
 import matter from "gray-matter";
 import { readdirSync, readFileSync } from "fs";
 import { NextPage } from "next";
+import Link from "next/link";
 
 interface Post {
   title: string;
   date: string;
   category: string;
+  slug: string;
 }
 
 const Blog: NextPage<{ posts: Post }> = ({ posts }) => {
-  console.log(posts);
   return (
     <Layout title="Blog" seoTitle="Blog">
       <h1 className="mb-10 mt-5 text-xl font-semibold">Latest Posts:</h1>
       <ul>
         {posts.map((post, index) => (
           <div key={index} className="mb-5">
-            <span className="text-lg text-red-500">{post.title}</span>
-            <div>
-              <span>
-                {post.date} / {post.category}
-              </span>
-            </div>
+            <Link href={`/blog/${post.slug}`}>
+              <div>
+                <span className="text-lg text-red-500">{post.title}</span>
+                <div>
+                  <span>
+                    {post.date} / {post.category}
+                  </span>
+                </div>
+              </div>
+            </Link>
           </div>
         ))}
       </ul>
@@ -33,10 +38,11 @@ const Blog: NextPage<{ posts: Post }> = ({ posts }) => {
 export async function getStaticProps() {
   const blogPosts = readdirSync("./posts").map((file) => {
     const content = readFileSync(`./posts/${file}`, "utf-8");
-    return matter(content).data;
+    const [slug, _] = file.split(".");
+    return { ...matter(content).data, slug };
   });
   return {
-    props: { posts: blogPosts },
+    props: { posts: blogPosts.reverse() },
   };
 }
 
