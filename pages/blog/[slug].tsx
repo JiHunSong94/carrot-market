e37abb37daaed7dc757282da6501759a8236eea9
Jihun Song <songjihun940409@gmail.com>
2022-12-21 19:31:15 +0900

    16.13 NEXTJS DEEP DIVE - Dynamic getStaticProps

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,8 +1,13 @@
 import { readdirSync } from "fs";
+import matter from "gray-matter";
 import { NextPage } from "next";
+import { GetStaticProps } from "next";
+import remarkHtml from "remark-html";
+import remarkParse from "remark-parse";
+import { unified } from "unified";
 
-const Post: NextPage = () => {
-  return <h1>hi</h1>;
+const Post: NextPage<{ post: string }> = ({ post }) => {
+  return <h1>{post}</h1>;
 };
 
 export function getStaticPaths() {
@@ -10,16 +15,21 @@ export function getStaticPaths() {
     const [name, extension] = file.split(".");
     return { params: { slug: name } };
   });
-  console.log(files);
   return {
     paths: files,
     fallback: false,
   };
 }
 
-export function getStaticProps() {
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { content } = matter.read(`./posts/${context.params?.slug}.md`);
+  const html = await unified()
+    .use(remarkParse)
+    .use(remarkHtml)
+    .process(content);
   return {
-    props: {},
+    props: { post: String(html) },
   };
-}
+};
+
 export default Post;
