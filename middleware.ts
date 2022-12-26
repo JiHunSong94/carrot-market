3c85a28cf1fb8269d501943c85b443dfb5375a68
Jihun Song <songjihun940409@gmail.com>
2022-12-26 22:04:41 +0900

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,6 @@ import type { NextRequest, NextFetchEvent } from "next/server";
 import { NextResponse, userAgent } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  if (userAgent(req).isBot) {
-    req.nextUrl.searchParams.set("from", req.nextUrl.pathname);
-    req.nextUrl.pathname = "/pages/_error";
-    return NextResponse.redirect(req.nextUrl);
-  }
   if (
     !req.cookies.has("carrotsession") &&
     !req.nextUrl.pathname.includes("/enter")
